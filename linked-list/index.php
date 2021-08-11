715a7219250c23e0cb6508978a688c1309693b9d
a-rudenko <ali.aksey.rudenko@gmail.com>
2021-08-11 19:43:58 +0300

    Adding linked list functionality

---
+++ linked-list/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'LinkedList.php';
+
+$array = range(1,20);
+$linkedList = new LinkedList();
+$linkedList->createLinkedList($array);
+$linkedList->addNode(21);
+echo 'Linked list:<br>' . $linkedList->printList() . '<br>';
+echo 'Head value is - ' . $linkedList->getHeadValue() . '<br><br>';
+$linkedList->reverseLinkedList();
+echo 'Reversing linked list:<br>' . $linkedList->printList() . '<br>';
+echo 'Head value is - ' . $linkedList->getHeadValue() . '<br><br>';
+echo $linkedList->findNode(21) . '<br><br>';
+$linkedList->removeNode(21);
+echo 'Linked list:<br>' . $linkedList->printList() . '<br>';
