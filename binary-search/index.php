d7a2a9c5633dad440232a2adf4c2563d0329b9e0
a-rudenko <ali.aksey.rudenko@gmail.com>
2021-08-16 19:34:52 +0300

    Adding some fixes

--- binary-search/index.php
+++ binary-search/index.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'Functions.php';
 
-$array = range(1, 10);
-$value = 4;
+$array = [1, 2, 3, 4, 5, 6, 7, '8', '9', 'test1', 'test2', 'test3', 'test4'];
+$value = 'test1';
 
 printResult(binarySearch($array, $value), $value);
 printResult(binarySearchRecursive($array, $value), $value);
